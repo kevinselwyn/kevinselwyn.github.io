0b4348ff3a1c322674fcc2aae2ac1c3069f866cc
Kevin Selwyn <kevinselwyn@gmail.com>
2020-06-06 17:04:48 -0400

    AUTO-GENERATING: 2020-06-06T21:04:48+0000

--- js/posts/doom-fire-in-react/react-doom-fire.js
+++ js/posts/doom-fire-in-react/react-doom-fire.js
@@ -1,1 +1,253 @@
-"use strict";function _typeof(obj){if(typeof Symbol==="function"&&typeof Symbol.iterator==="symbol"){_typeof=function _typeof(obj){return typeof obj}}else{_typeof=function _typeof(obj){return obj&&typeof Symbol==="function"&&obj.constructor===Symbol&&obj!==Symbol.prototype?"symbol":typeof obj}}return _typeof(obj)}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function")}}function _defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor)}}function _createClass(Constructor,protoProps,staticProps){if(protoProps)_defineProperties(Constructor.prototype,protoProps);if(staticProps)_defineProperties(Constructor,staticProps);return Constructor}function _possibleConstructorReturn(self,call){if(call&&(_typeof(call)==="object"||typeof call==="function")){return call}return _assertThisInitialized(self)}function _getPrototypeOf(o){_getPrototypeOf=Object.setPrototypeOf?Object.getPrototypeOf:function _getPrototypeOf(o){return o.__proto__||Object.getPrototypeOf(o)};return _getPrototypeOf(o)}function _assertThisInitialized(self){if(self===void 0){throw new ReferenceError("this hasn't been initialised - super() hasn't been called")}return self}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function")}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,writable:true,configurable:true}});if(superClass)_setPrototypeOf(subClass,superClass)}function _setPrototypeOf(o,p){_setPrototypeOf=Object.setPrototypeOf||function _setPrototypeOf(o,p){o.__proto__=p;return o};return _setPrototypeOf(o,p)}var ReactDoomFire=function(_React$Component){_inherits(ReactDoomFire,_React$Component);function ReactDoomFire(props){var _this;_classCallCheck(this,ReactDoomFire);_this=_possibleConstructorReturn(this,_getPrototypeOf(ReactDoomFire).call(this,props));_this.canvas=React.createRef();_this.context=null;_this.data=null;_this.pixels=[];_this.animate=null;_this.time=Date.now();_this.interval=1e3/props.fps;_this._onInit=_this._onInit.bind(_assertThisInitialized(_this));_this._onPixelsInit=_this._onPixelsInit.bind(_assertThisInitialized(_this));_this._onCanvasInit=_this._onCanvasInit.bind(_assertThisInitialized(_this));_this._onStart=_this._onStart.bind(_assertThisInitialized(_this));_this._onStop=_this._onStop.bind(_assertThisInitialized(_this));_this._onRun=_this._onRun.bind(_assertThisInitialized(_this));_this._onDraw=_this._onDraw.bind(_assertThisInitialized(_this));return _this}_createClass(ReactDoomFire,[{key:"componentDidMount",value:function componentDidMount(){var _this$props=this.props,width=_this$props.width,height=_this$props.height,running=_this$props.running;var canvas=this.canvas;this.context=canvas.current.getContext("2d");this.data=this.context.getImageData(0,0,width,height);this._onInit();if(running){this._onStart()}}},{key:"componentDidUpdate",value:function componentDidUpdate(prevProps){var _this$props2=this.props,width=_this$props2.width,height=_this$props2.height,running=_this$props2.running,fps=_this$props2.fps;if(!(this.context instanceof CanvasRenderingContext2D)){this.context=this.canvas.current.getContext("2d");this.data=this.context.getImageData(0,0,width,height)}if(prevProps.running!==running&&running){this._onStart()}if(prevProps.fps!==fps){this.interval=1e3/fps}if(prevProps.width!==width||prevProps.height!==height){this.data=this.context.getImageData(0,0,width,height);this._onInit()}}},{key:"_onInit",value:function _onInit(){this._onPixelsInit();this._onCanvasInit()}},{key:"_onPixelsInit",value:function _onPixelsInit(){var _this$props3=this.props,width=_this$props3.width,height=_this$props3.height;var size=width*height;var i=0;var l=0;this.pixels=this.pixels.slice(0,size);if(this.pixels.length<size){this.pixels=this.pixels.concat(new Array(size-this.pixels.length))}for(i=0,l=size;i<l;i+=1){this.pixels[i]=0}}},{key:"_onCanvasInit",value:function _onCanvasInit(){var _this$props4=this.props,width=_this$props4.width,height=_this$props4.height,palette=_this$props4.palette,transparent=_this$props4.transparent;var canvas=this.canvas,context=this.context;canvas.current.setAttribute("width",width);canvas.current.setAttribute("height",height);if(transparent){context.clearRect(0,0,width,height)}else{context.fillStyle="#".concat(palette[0].toString(16));context.fillRect(0,0,width,height)}}},{key:"_onStart",value:function _onStart(){var _this$props5=this.props,width=_this$props5.width,height=_this$props5.height,palette=_this$props5.palette;var length=palette.length;var x=0;var y=height-1;for(x=0;x<width;x+=1){this.pixels[x+y*width]=length-1}this._onRun()}},{key:"_onStop",value:function _onStop(){this.animate=cancelAnimationFrame(this.animate)}},{key:"_onRun",value:function _onRun(){var interval=this.interval;this.animate=cancelAnimationFrame(this.animate);this.animate=requestAnimationFrame(this._onRun);var now=Date.now();var elapsed=now-this.time;if(elapsed>interval){this.time=now-elapsed%interval;this._onDraw()}}},{key:"_onDraw",value:function _onDraw(){var _this$props6=this.props,width=_this$props6.width,height=_this$props6.height,running=_this$props6.running,palette=_this$props6.palette,transparent=_this$props6.transparent;var context=this.context,data=this.data;var total=0;var x=0;var y=0;context.clearRect(0,0,width,height);for(x=0;x<width;x+=1){for(y=1;y<height;y+=1){var src=x+y*width;var pixel=this.pixels[src];if(pixel===0){this.pixels[src-width]=0}else{var rand=Math.round(Math.random()*3)&3;var dest=src-rand+1;var val=pixel-(rand&1);this.pixels[dest-width]=val;total+=val}}}if(!running){for(y=height-1;y>height-8;y-=1){for(x=0;x<width;x+=1){var _rand=Math.round(Math.random()*3)&3;var _val=Math.max(this.pixels[x+y*width]-_rand,0);this.pixels[x+y*width]=_val;total+=_val}}}for(y=0;y<height;y+=1){for(x=0;x<width;x+=1){var index=this.pixels[x+y*width];var _pixel=palette[index];data.data[(x+y*width)*4+0]=_pixel>>16&255;data.data[(x+y*width)*4+1]=_pixel>>8&255;data.data[(x+y*width)*4+2]=_pixel&255;data.data[(x+y*width)*4+3]=transparent?index===0?0:255:255}}context.putImageData(data,0,0);if(!total){this._onStop()}}},{key:"render",value:function render(){return React.createElement("canvas",{width:this.props.width,height:this.props.height,ref:this.canvas})}}]);return ReactDoomFire}(React.Component);ReactDoomFire.propTypes={width:PropTypes.number,height:PropTypes.number,fps:PropTypes.number,palette:PropTypes.arrayOf(PropTypes.number),running:PropTypes["boolean"],transparent:PropTypes["boolean"]};ReactDoomFire.defaultProps={width:320,height:168,fps:27,palette:[460551,2033415,3084039,4656903,5707527,6758151,7806727,9381639,10432263,11484935,12535559,13059847,14634759,14636807,14636807,14114567,14114567,14116623,13594383,13596431,13598479,13600535,13076247,13078295,13080351,12558111,12558111,12560167,12560167,12562223,12037935,12039983,12039991,13619055,14671775,15724487,16777215],running:false,transparent:false};
+class ReactDoomFire extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.canvas = React.createRef();
+        this.context = null;
+
+        this.data = null;
+        this.pixels = [];
+
+        this.animate = null;
+        this.time = Date.now();
+        this.interval = 1000 / props.fps;
+
+        this._onInit = this._onInit.bind(this);
+        this._onPixelsInit = this._onPixelsInit.bind(this);
+        this._onCanvasInit = this._onCanvasInit.bind(this);
+        this._onStart = this._onStart.bind(this);
+        this._onStop = this._onStop.bind(this);
+        this._onRun = this._onRun.bind(this);
+        this._onDraw = this._onDraw.bind(this);
+    }
+
+    componentDidMount() {
+        const {width, height, running} = this.props;
+        const {canvas} = this;
+
+        this.context = canvas.current.getContext('2d');
+        this.data = this.context.getImageData(0, 0, width, height);
+
+        this._onInit();
+
+        if (running) {
+            this._onStart();
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        const {width, height, running, fps} = this.props;
+
+        if (!(this.context instanceof CanvasRenderingContext2D)) {
+            this.context = this.canvas.current.getContext('2d');
+            this.data = this.context.getImageData(0, 0, width, height);
+        }
+
+        if (prevProps.running !== running && running) {
+            this._onStart();
+        }
+
+        if (prevProps.fps !== fps) {
+            this.interval = 1000 / fps;
+        }
+
+        if (prevProps.width !== width || prevProps.height !== height) {
+            this.data = this.context.getImageData(0, 0, width, height);
+            this._onInit();
+        }
+    }
+
+    _onInit() {
+        this._onPixelsInit();
+        this._onCanvasInit();
+    }
+
+    _onPixelsInit() {
+        const {width, height} = this.props;
+        const size = width * height;
+        var i = 0;
+        var l = 0;
+
+        this.pixels = this.pixels.slice(0, size);
+
+        if (this.pixels.length < size) {
+            this.pixels = this.pixels.concat(new Array(size - this.pixels.length));
+        }
+
+        for (i = 0, l = size; i < l; i += 1) {
+            this.pixels[i] = 0;
+        }
+    }
+
+    _onCanvasInit() {
+        const {width, height, palette, transparent} = this.props;
+        const {canvas, context} = this;
+
+        canvas.current.setAttribute('width', width);
+        canvas.current.setAttribute('height', height);
+
+        if (transparent) {
+            context.clearRect(0, 0, width, height);
+        } else {
+            context.fillStyle = `#${palette[0].toString(16)}`;
+            context.fillRect(0, 0, width, height);
+        }
+    }
+
+    _onStart() {
+        const {width, height, palette} = this.props;
+        const {length} = palette;
+        var x = 0;
+        var y = height - 1;
+
+        for (x = 0; x < width; x += 1) {
+            this.pixels[x + (y * width)] = length - 1;
+        }
+
+        this._onRun();
+    }
+
+    _onStop() {
+        this.animate = cancelAnimationFrame(this.animate);
+    }
+
+    _onRun() {
+        const {interval} = this;
+
+        this.animate = cancelAnimationFrame(this.animate);
+        this.animate = requestAnimationFrame(this._onRun);
+
+        const now = Date.now();
+        const elapsed = now - this.time;
+
+        if (elapsed > interval) {
+            this.time = now - (elapsed % interval);
+
+            this._onDraw();
+        }
+    }
+
+    _onDraw() {
+        const {width, height, running, palette, transparent} = this.props;
+        const {context, data} = this;
+        var total = 0;
+        var x = 0;
+        var y = 0;
+
+        context.clearRect(0, 0, width, height);
+
+        for (x = 0; x < width; x += 1) {
+            for (y = 1; y < height; y += 1) {
+                const src = x + (y * width);
+                const pixel = this.pixels[src];
+
+                if (pixel === 0) {
+                    this.pixels[src - width] = 0;
+                } else {
+                    const rand = Math.round(Math.random() * 3) & 3;
+                    const dest = src - rand + 1;
+                    const val = pixel - (rand & 1);
+
+                    this.pixels[dest - width] = val;
+
+                    total += val;
+                }
+            }
+        }
+
+        if (!running) {
+            for (y = height - 1; y > height - 8; y -= 1) {
+                for (x = 0; x < width; x += 1) {
+                    const rand = Math.round(Math.random() * 3) & 3;
+                    const val = Math.max(this.pixels[x + (y * width)] - rand, 0);
+
+                    this.pixels[x + (y * width)] = val;
+
+                    total += val;
+                }
+            }
+        }
+
+        for (y = 0; y < height; y += 1) {
+            for (x = 0; x < width; x += 1) {
+                const index = this.pixels[x + (y * width)];
+                const pixel = palette[index];
+
+                data.data[((x + (y * width)) * 4) + 0] = (pixel >> 16) & 0xFF;
+                data.data[((x + (y * width)) * 4) + 1] = (pixel >> 8) & 0xFF;
+                data.data[((x + (y * width)) * 4) + 2] = pixel & 0xFF;
+                data.data[((x + (y * width)) * 4) + 3] = transparent ? ((index === 0) ? 0 : 255) : 255;
+            }
+        }
+
+        context.putImageData(data, 0, 0);
+
+        if (!total) {
+            this._onStop();
+        }
+    }
+
+    render() {
+        return React.createElement('canvas', {
+            width: this.props.width,
+            height: this.props.height,
+            ref: this.canvas
+        });
+    }
+}
+
+ReactDoomFire.propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    fps: PropTypes.number,
+    palette: PropTypes.arrayOf(PropTypes.number),
+    running: PropTypes.boolean,
+    transparent: PropTypes.boolean
+};
+
+ReactDoomFire.defaultProps = {
+    width: 320,
+    height: 168,
+    fps: 27,
+    palette: [
+        0x070707,
+        0x1F0707,
+        0x2F0F07,
+        0x470F07,
+        0x571707,
+        0x671F07,
+        0x771F07,
+        0x8F2707,
+        0x9F2F07,
+        0xAF3F07,
+        0xBF4707,
+        0xC74707,
+        0xDF4F07,
+        0xDF5707,
+        0xDF5707,
+        0xD75F07,
+        0xD75F07,
+        0xD7670F,
+        0xCF6F0F,
+        0xCF770F,
+        0xCF7F0F,
+        0xCF8717,
+        0xC78717,
+        0xC78F17,
+        0xC7971F,
+        0xBF9F1F,
+        0xBF9F1F,
+        0xBFA727,
+        0xBFA727,
+        0xBFAF2F,
+        0xB7AF2F,
+        0xB7B72F,
+        0xB7B737,
+        0xCFCF6F,
+        0xDFDF9F,
+        0xEFEFC7,
+        0xFFFFFF
+    ],
+    running: false,
+    transparent: false
+};
