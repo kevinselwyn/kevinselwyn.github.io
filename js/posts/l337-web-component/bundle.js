e1a8de1cda9e633612ac837221ba9429b612671f
Kevin Selwyn <kevinselwyn@gmail.com>
2020-06-12 17:23:38 -0400

    AUTO-GENERATING: 2020-06-12T21:23:38+0000

--- js/posts/l337-web-component/bundle.js
+++ js/posts/l337-web-component/bundle.js
@@ -1,14 +1,1 @@
-(function () {
-    var l337 = document.querySelector('.l337-5p34k');
-
-    if (!l337) {
-        return;
-    }
-
-    var l337_input = l337.querySelector('input');
-    var l337_el = l337.querySelector('l337-5p34k');
-
-    l337_input.addEventListener('keyup', function () {
-        l337_el.innerText = l337_input.value;
-    });
-}());
+"use strict";(function(){var l337=document.querySelector(".l337-5p34k");if(!l337){return}var l337_input=l337.querySelector("input");var l337_el=l337.querySelector("l337-5p34k");l337_input.addEventListener("keyup",function(){l337_el.innerText=l337_input.value})})();
